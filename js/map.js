7f91bf889e3438496ee145979168d917c1330812
jacobhackett <jrhackett@wpi.edu>
2016-01-23 00:05:45 -0500

    working on some animation stuff for drawing circles

--- js/map.js
+++ js/map.js
@@ -1,11 +1,46 @@
 
-
+var draw_circle=null;
+var flag = 1;
 function initMap() {
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 33.87041555, lng: -23.81835937},
     scrollwheel: false,
-    zoom: 8
+    zoom: 2
   });
+
+	draw_circle = new google.maps.Circle({
+		center: new google.maps.LatLng(33.87041555, -23.81835937),
+		radius: 700000,
+		strokeColor: "red",
+		strokeOpacity: 0.8,
+		strokeWeight: 2,
+		fillColor: "red",
+		fillOpacity: 0.35,
+		map: map
+	});
+	loop();
 }
+
+function loop() {
+	//console.log("hi");
+	setTimeout(function() {
+		if(draw_circle.radius > 10000 && flag === 1) {
+			draw_circle.radius-=10000;
+			console.log("subtract");
+		}
+		else if(draw_circle.radius < 700000 && flag === 0) {
+			draw_circle.radius+=10000;
+			console.log("add");
+		}
+		else {
+			if(flag === 1){flag = 0;}else{flag = 1;}
+			console.log(flag, draw_circle.radius);
+		}
+
+		loop();
+	}, 100)
+}
+
+
 
